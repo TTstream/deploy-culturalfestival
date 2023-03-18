e22a61a9e80f77152dc366e0f95b3823ef02bf77
SeongJin Oh <guk0230@naver.com>
2023-03-18 14:27:40 +0900

    필터링 한 데이터 화면 출력

--- src/main/java/cultural/festival/controller/MainController.java
+++ src/main/java/cultural/festival/controller/MainController.java
@@ -1,14 +1,28 @@
 package cultural.festival.controller;
 
+import cultural.festival.entity.Api;
+import cultural.festival.repository.ApiRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class MainController {
+import java.util.List;
 
+@Controller
+@RequiredArgsConstructor
+public class MainController{
+
+    private final ApiRepository apiRepository;
     @GetMapping("/publicData")
-    public String index(){
+    public String index(Model model){
+
+        // 2021년 이후의 데이터를 내림차순으로 출력(최신 데이터부터 출력)
+        List<Api> apiDataList=apiRepository.fstvlStart2021_();
+        model.addAttribute("apiDataList",apiDataList);
         return "index";
+
     }
 
 }
--- src/main/java/cultural/festival/repository/ApiRepository.java
+++ src/main/java/cultural/festival/repository/ApiRepository.java
@@ -2,6 +2,13 @@ package cultural.festival.repository;
 
 import cultural.festival.entity.Api;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ApiRepository extends JpaRepository<Api,Long> {
+    
+    // 2021년 이후의 데이터들을 축제시작일 기준으로 내림차순해서 출력(즉, 최신 데이터부터 출력)
+    @Query("select i from Api i where i.fstvlStartDate>=2021 order by i.fstvlStartDate desc")
+    List<Api> fstvlStart2021_();
 }
